0b06e1cb463e03ea84e8e6acf49737988f9a083e
rahul <rahulor2000@gmail.com>
2023-08-25 23:25:03 +0530

    completed all cart functionalities with redux

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import newsReducer from './newsReducer';
+import cartReducer from './cartReducer';
 
 const rootReducer = combineReducers({
   newsDetails: newsReducer,
+  cart: cartReducer,
 })
 
 export default rootReducer;
