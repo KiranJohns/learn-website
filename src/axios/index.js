68eff417c8e13f758327ebf5ac945339c1733643
rahul <rahulor2000@gmail.com>
2023-09-21 15:00:53 +0530

    signuppage updated

--- src/axios/index.js
+++ src/axios/index.js
@@ -4,43 +4,33 @@ import store from "../redux/store";
 
 export default function useFetch() {
   let token = null;
-
   let BASEURL = "";
-  BASEURL = "http://localhost:3001/api";
+  BASEURL = "https://www.testkiran.online/api";
 
   function makeRequest(method, url, data = {}) {
-    console.log(method);
-    console.log(BASEURL, url);
-    store.dispatch({
-        type: 'SET_LOADING'
-    })
-    try {
-      axios({
-        method,
-        url: `${BASEURL}${url}`,
-        data: JSON.stringify(data),
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        withCredentials: true,
-      })
-        .then((res) => {
-            store.dispatch({
-                type: "SET_RESPONSE",
-                payload: res
-            })
+    return new Promise((resolve, reject) => {
+      try {
+        axios({
+          method,
+          url: `${BASEURL}${url}`,
+          data: JSON.stringify(data),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          withCredentials: true,
         })
-        .catch((error) => {
-            store.dispatch({
-                type: "SET_ERROR",
-                payload: error
-            })
-          console.log(error?.response?.data);
-        });
-    } catch (error) {
-      console.log(error?.message);
-    }
+          .then((res) => {
+            resolve(res.data)
+          })
+          .catch((error) => {
+            reject(error.response)
+            console.log(error?.response?.data);
+          });
+      } catch (error) {
+        reject(error?.message);
+      }
+    });
   }
   return makeRequest;
 }
