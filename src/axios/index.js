ca778bfb1f11198bfa4e9f93ce6a2f4bd9838541
rahul <rahulor2000@gmail.com>
2023-09-21 13:12:04 +0530

    signup page functionality finished

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,56 +1,46 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-
+import React, { useState } from "react";
+import axios from "axios";
+import store from "../redux/store";
 
 export default function useFetch() {
+  let token = null;
 
-    let token = null;
+  let BASEURL = "";
+  BASEURL = "http://localhost:3001/api";
 
-    let BASEURL = ""
-    BASEURL = "https://www.testkiran.online/api"
-
-    const [response, setResponse] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-
-    function makeRequest(method, url, data = {}) {
-        console.log(method);
-        setLoading(true)
-        setResponse(null);
-        setError(null);
-        console.log(BASEURL, url)
-        try {
-            axios({
-                method,
-                url: `${BASEURL}${url}`,
-                data: JSON.stringify(data),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                },
-                withCredentials: true
-
-            }).then(res => {
-                setLoading(false)
-                setResponse(res?.data);
-                setError(null);
-                console.log(res);
-            }).catch(error => {
-                setLoading(false)
-                setResponse(null);
-                setError(error?.response?.data);
-                console.log(error?.response?.data);
+  function makeRequest(method, url, data = {}) {
+    console.log(method);
+    console.log(BASEURL, url);
+    store.dispatch({
+        type: 'SET_LOADING'
+    })
+    try {
+      axios({
+        method,
+        url: `${BASEURL}${url}`,
+        data: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        withCredentials: true,
+      })
+        .then((res) => {
+            store.dispatch({
+                type: "SET_RESPONSE",
+                payload: res
             })
-        } catch (error) {
-
-            setLoading(false)
-            setResponse(null);
-            setError(error?.message);
-            console.log(error?.message)
-
-        }
-       
-        
+        })
+        .catch((error) => {
+            store.dispatch({
+                type: "SET_ERROR",
+                payload: error
+            })
+          console.log(error?.response?.data);
+        });
+    } catch (error) {
+      console.log(error?.message);
     }
-    return[response, error, loading, makeRequest]
+  }
+  return makeRequest;
 }
