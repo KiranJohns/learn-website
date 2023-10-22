8e48727f1263f7f76d35b4c6f4edc9a1a36e7816
rahul <rahulor2000@gmail.com>
2023-10-22 20:31:10 +0530

    single blo page updated

--- src/components/Elements/Tab/CourseGridTab.js
+++ src/components/Elements/Tab/CourseGridTab.js
@@ -1,45 +1,62 @@
-import React, { Component } from 'react';
-import dynamic from 'next/dynamic';
-import store from '../../../redux/store'
-const Tabs = dynamic(import('react-tabs').then(mod => mod.Tabs), { ssr: false })
-import { Tab, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-import PaginationSection from '../../Common/Pagination';
-import Link from 'next/link';
-import sampleProducts from '../../../../sampleProduct.json'
-import { useSelector } from 'react-redux'
+import React, { Component, useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+import store from "../../../redux/store";
+const Tabs = dynamic(
+  import("react-tabs").then((mod) => mod.Tabs),
+  { ssr: false }
+);
+import { Tab, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import PaginationSection from "../../Common/Pagination";
+import Link from "next/link";
+import { useSelector } from "react-redux";
+import fetchData from '../../../axios/index'
 
 export default () => {
-    const { cart } = useSelector((store) => store.cart)
-    function addToCart(id) {
-      const item = sampleProducts.find((item) => item.id === id)
-      store.dispatch({
-        type: 'ADD_TO_CART',
-        payload: item,
-      })
-    }
+  const { cart } = useSelector((store) => store.cart);
+  let makeRequest = fetchData();
+
+  const [course, setCourse] = useState([])
+  function addToCart(id) {
+
+    store.dispatch({
+      type: "ADD_TO_CART",
+      payload: item,
+    });
+  }
+
+  function increment(id) {
+    store.dispatch({
+      type: "INCREMENT_ITEM_CONT",
+      payload: id,
+    });
+  }
+  function decrement(id) {
+    store.dispatch({
+      type: "DECREMENT_ITEM_CONT",
+      payload: id,
+    });
+  }
+
+  useEffect(() => {
+      makeRequest("GET", "/course/get-all-course")
+    .then((res) => {
+      setCourse(res.data.response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }, [])
   
-    function increment(id) {
-      store.dispatch({
-        type: 'INCREMENT_ITEM_CONT',
-        payload: id,
-      })
-    }
-    function decrement(id) {
-      store.dispatch({
-        type: 'DECREMENT_ITEM_CONT',
-        payload: id,
-      })
-    }
-   
-return(
+
+  return (
     <section className="course__area pt-115 pb-120 grey-bg">
-    <Tabs variant="enclosed" id="react-tabs-276">
-      <div className="container">
-        <div className="row align-items-end">
-          <div className="col-xxl-5 col-xl-6 col-lg-6">
-            <div className="section__title-wrapper mb-60">
-              {/* <h2 className="section__title">
+      <Tabs variant="enclosed" id="react-tabs-276">
+        <div className="container">
+          <div className="row align-items-end">
+            <div className="col-xxl-5 col-xl-6 col-lg-6">
+              <div className="section__title-wrapper mb-60">
+                {/* <h2 className="section__title">
                 Find the Right
                 <br />
                 Online{' '}
@@ -52,13 +69,13 @@ return(
                 </span>{' '}
                 for you
               </h2> */}
-              {/* <p>
+                {/* <p>
                 You don't have to struggle alone, you've got our assistance
                 and help.
               </p> */}
+              </div>
             </div>
-          </div>
-          {/* <div className="col-xxl-7 col-xl-6 col-lg-6">
+            {/* <div className="col-xxl-7 col-xl-6 col-lg-6">
                    <div className="course__menu d-flex justify-content-lg-end mb-60">
                       <div className="masonary-menu filter-button-group">
                          <TabList>
@@ -71,92 +88,88 @@ return(
                      </div>
                    </div>
                 </div> */}
-        </div>
-        <TabPanel>
-          <div className="row">
-            {sampleProducts.map((item) => (
-              <div
-                key={item.id}
-                className="col-xxl-4 col-xl-4 col-lg-4 col-md-6"
-              >
-                <div className="course__item white-bg mb-30 fix">
-                  <div className="course__thumb w-img p-relative fix">
-                    <Link href="/course-details">
-                      <a>
-                        <img src={item.image} alt="img not found" />
-                      </a>
-                    </Link>
-                    {/* <div className="course__tag">
+          </div>
+          <TabPanel>
+            <div className="row">
+              {course.map((item) => (
+                <div
+                  key={item.id}
+                  className="col-xxl-4 col-xl-4 col-lg-4 col-md-6"
+                >
+                  <div className="course__item white-bg mb-30 fix">
+                    <div className="course__thumb w-img p-relative fix">
+                      <Link href="/course-details">
+                        <a>
+                          <img src={item.thumbnail} alt="img not found" />
+                        </a>
+                      </Link>
+                      {/* <div className="course__tag">
                       <Link href="/course-details">
                         <a className="orange">{item.course_tags}</a>
                       </Link>
                     </div> */}
-                  </div>
-                  <div className="course__content">
-                    <h3 className="course__title">
-                      <Link href={`/course/${item.id}`}>
-                        <a>{item.heading}</a>
-                      </Link>
-                    </h3>
-                    <div className="course__teacher d-flex align-items-center">
-                      {/* <div className="course__teacher-thumb mr-15">
+                    </div>
+                    <div className="course__content">
+                      <h3 className="course__title">
+                        <Link href={`/course/${item.id}`}>
+                          <a>{item.name}</a>
+                        </Link>
+                      </h3>
+                      <div className="course__teacher d-flex align-items-center">
+                        {/* <div className="course__teacher-thumb mr-15">
                                <img src="assets/img/course/teacher/teacher-5.jpg" alt="img not found"/>
                             </div> */}
-                      <h6>
-                        <Link href={`/course/${item.id}`}>
-                          <a>{item.description}</a>
-                        </Link>
-                      </h6>
-                    </div>
-                  </div>
-                  <div className="course__more d-flex justify-content-between ">
-                    <div className="course__status d-flex align-items-center">
-                      <span className="sky-blue">£{item.price}</span>
-                    </div>
-                    <span>
-                      <div className="d-flex">
-                        <button
-                          className="cart-minus"
-                          onClick={() => decrement(item.id)}
-                        >
-                          <i className="fas fa-minus"></i>
-                        </button>
-                        <p className="p-1">
-                          {(cart &&
-                            cart?.find((cartItem) => cartItem.id === item.id)
-                              ?.count) ||
-                            0}
-                        </p>
-                        <button
-                          className="cart-plus"
-                          onClick={() => increment(item.id)}
-                        >
-                          <i className="fas fa-plus"></i>
-                        </button>
+                        <h6>
+                          <Link href={`/course/${item.id}`}>
+                            <a>{item.description}</a>
+                          </Link>
+                        </h6>
                       </div>
-                    </span>
-                    <span>
-                      <button
-                        className="course__more d-flex justify-content-between align-items-center"
-                        type="button"
-                        class="btn btn-primary btn-sm"
-                        onClick={() => addToCart(item.id)}
-                      >
-                        Add to cart
-                      </button>
-                    </span>
+                    </div>
+                    <div className="course__more d-flex justify-content-between ">
+                      <div className="course__status d-flex align-items-center">
+                        <span className="sky-blue">£{item.price}</span>
+                      </div>
+                      <span>
+                        <div className="d-flex">
+                          <button
+                            className="cart-minus"
+                            onClick={() => decrement(item.id)}
+                          >
+                            <i className="fas fa-minus"></i>
+                          </button>
+                          <p className="p-1">
+                            {(cart &&
+                              cart?.find((cartItem) => cartItem.id === item.id)
+                                ?.count) ||
+                              0}
+                          </p>
+                          <button
+                            className="cart-plus"
+                            onClick={() => increment(item.id)}
+                          >
+                            <i className="fas fa-plus"></i>
+                          </button>
+                        </div>
+                      </span>
+                      <span>
+                        <button
+                          className="course__more d-flex justify-content-between align-items-center"
+                          type="button"
+                          class="btn btn-primary btn-sm"
+                          onClick={() => addToCart(item.id)}
+                        >
+                          Add to cart
+                        </button>
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        </TabPanel>
-     
-       
-      
-       
-      </div>
-    </Tabs>
-  </section>
-);
-                          }
+              ))}
+            </div>
+          </TabPanel>
+        </div>
+      </Tabs>
+    </section>
+  );
+};
