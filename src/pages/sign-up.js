a6205133ea97d23e8b77191ebad8c08f96135020
Kiran Johnson <mekiranjohnson@gmail.com>
2023-10-25 11:54:44 +0530

    no change

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -4,6 +4,7 @@ import HeaderFour from '../components/Layout/Header/HeaderStyleFour';
 import SignUpMain from '../components/SignUp/SignUpMain';
 import Header from '../components/Layout/Header/Header';
 import Footer from '../components/Layout/Footer/Footer';
+import NoSSR from 'react-no-ssr';
 
 class SignIn extends React.Component {
 
@@ -16,7 +17,9 @@ class SignIn extends React.Component {
     render() {
         return (
             <React.Fragment>
+            <NoSSR>
             <Header/>
+            </NoSSR>
             <SignUpMain />
             <Footer />
             </React.Fragment>
