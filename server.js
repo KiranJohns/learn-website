1bacfddd505c8ef2ce1f9350f473a43968a1663d
Kiran Johnson <mekiranjohnson@gmail.com>
2023-11-03 13:38:36 +0530

    push

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-
 app.prepare().then(() => {
   const server = express()
 
@@ -17,5 +16,5 @@ app.prepare().then(() => {
   server.listen(port, (err) => {
     if (err) throw err
     console.log(`> Ready on http://localhost:${port}`)
-  })
+  })
 })
