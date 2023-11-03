9055f12be567264aa4106518deaa024580d28624
Kiran Johnson <mekiranjohnson@gmail.com>
2023-11-03 18:25:43 +0530

    profile picture

--- src/components/Home/DashProfile.js
+++ src/components/Home/DashProfile.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
@@ -7,100 +7,101 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 
+function DashProfile() {
 
-class DashProfile extends Component {
-  state = {
-    info: {
-      username: "",
-      email: "",
-      address: "",
-      city: "",
-      state: "",
-      phone: "",
-    }
-  }
-  componentDidMount() {
-    let makeRequest = fetchData()
+ const handleImage =()=>{
+ inputRef.current.click()
+ }
+
+
+  const inputRef = useRef(null);
+
+  const [info, setInfo] = useState({
+    username: "",
+    email: "",
+    address: "",
+    city: "",
+    state: "",
+    phone: "",
+  });
+
+  useEffect(() => {
+    const makeRequest = fetchData();
     console.log('hi');
-    makeRequest("GET","/info/data").then(res => {
-      console.log(res);
-    }).catch(err => {
-      console.log(err);
-    })
-  }
-  render() {
-    return (
-      <div className="">
-        <h2
-          style={{
-            padding: "1.5rem",
-            color: "#004b55",
-            display: "flex",
-            justifyContent: "center",
-            marginTop: "20px",
-            fontSize: 46,
-          }}
-        >
-          My Profile
-        </h2>
-        <div> <Container>
-      <Row>
-      
-        <Col xs={6} md={4}>
-          <Image src="/assets/img/testimonial/testimonial-3.jpg" roundedCircle />
-        </Col>
-       
-      </Row>
-    </Container></div>
-        <Form style={{ padding: "20px" }}>
-          <Row className="mb-3 mt-4">
-            <Form.Group as={Col} controlId="formGridEmail">
-              <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Name" />
-            </Form.Group>
+    makeRequest("GET", "/info/data")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-            <Form.Group as={Col} controlId="formGridPassword">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Email" />
-            </Form.Group>
-          </Row>
-
-          <Form.Group className="mb-3" controlId="formGridAddress1">
-            <Form.Label>Address</Form.Label>
-            <Form.Control placeholder="1234 Main St" />
+  return (
+    <div className="">
+      <h2
+        style={{
+          padding: "1.5rem",
+          color: "#004b55",
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "20px",
+          fontSize: 46,
+        }}
+      >
+        My Profile
+      </h2>
+      <div style={{ padding: "0 20px" }} onClick={handleImage}>
+        <img style={{width:'70px',height:'70px',borderRadius:'70px',cursor:'pointer'}} src="/assets/img/testimonial/profilePic.webp" alt="" />
+        <input type="file" ref={inputRef} style={{display:'none'}}/>
+      </div>
+      <Form style={{ padding: "0px 20px" }}>
+        <Row className="mb-3 mt-4">
+          <Form.Group as={Col} controlId="formGridEmail">
+            {/* <Form.Label>Name</Form.Label> */}
+            <Form.Control type="text" placeholder="Name" />
           </Form.Group>
 
-          <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridCity">
-              <Form.Label>City</Form.Label>
-              <Form.Control />
-            </Form.Group>
+          <Form.Group as={Col} controlId="formGridPassword">
+            {/* <Form.Label>Email</Form.Label> */}
+            <Form.Control type="email" placeholder="Email" />
+          </Form.Group>
+        </Row>
 
-            <Form.Group as={Col} controlId="formGridState">
-              <Form.Label>Country</Form.Label>
-              <Form.Select defaultValue="Choose...">
-                <option>Choose...</option>
-                <option>...</option>
-              </Form.Select>
-            </Form.Group>
+        <Form.Group className="mb-3" controlId="formGridAddress1">
+          <Form.Label>Address</Form.Label>
+          <Form.Control placeholder="1234 Main St" />
+        </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridZip">
-              <Form.Label>Phone</Form.Label>
-              <Form.Control />
-            </Form.Group>
-          </Row>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formGridCity">
+            <Form.Label>City</Form.Label>
+            <Form.Control />
+          </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Update
-          </Button>
-        </Form>
-      </div>
-    );
-  }
+          <Form.Group as={Col} controlId="formGridState">
+            <Form.Label>Country</Form.Label>
+            <Form.Select defaultValue="Choose...">
+              <option>Choose...</option>
+              <option>...</option>
+            </Form.Select>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridZip">
+            <Form.Label>Phone</Form.Label>
+            <Form.Control />
+          </Form.Group>
+        </Row>
+
+        <Button variant="primary" type="submit">
+          Update
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default DashProfile;
-
 // import React, {Component} from "react";
 
 // class DashProfile extends Component{
