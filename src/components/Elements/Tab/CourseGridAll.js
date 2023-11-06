049566947deaf1020910a60a88845f80da6c0cc0
rahul <rahulor2000@gmail.com>
2023-11-06 17:00:28 +0530

    signup page updated

--- src/components/Elements/Tab/CourseGridAll.js
+++ src/components/Elements/Tab/CourseGridAll.js
@@ -14,7 +14,6 @@ import fetchData from "../../../axios/index";
 import CourseCard from "./CourseCard";
 import ResponsivePagination from "react-responsive-pagination";
 
-
 export default () => {
   const { cart } = useSelector((store) => store.cart);
   let makeRequest = fetchData();
@@ -24,14 +23,18 @@ export default () => {
   const [count, setCount] = useState(0);
   const [selectedCount, setSelectedCount] = useState(1);
 
+  const [searchText, setSearchText] = useState("");
+
   function getCourse(limit) {
-    if(limit == 1) {
-      limit = 0
+    if (limit == 1) {
+      limit = 0;
     } else {
-      setSelectedCount(limit)
-      limit = 12 * limit
-      --limit
+      setSelectedCount(limit);
+      limit = 12 * limit;
+      --limit;
     }
+
+    useEffect(() => {}, [searchText]);
     makeRequest("GET", `/course/get-course-by-limit/${limit}`)
       .then((res) => {
         setCourse(res.data.response.courses);
@@ -43,15 +46,14 @@ export default () => {
   }
 
   function handleClick(val) {
-    getCourse(val)
+    getCourse(val);
   }
-
-
 
   useEffect(() => {
     makeRequest("GET", `/course/get-course-by-limit/${count}`)
       .then((res) => {
         console.log(res);
+        console.log(res.data.response.courses);
         setCourse(res.data.response.courses);
         setCount(res.data.response.count);
       })
@@ -64,6 +66,18 @@ export default () => {
     <section className="course__area pt-50 pb-60 grey-bg">
       <Tabs variant="enclosed" id="react-tabs-276">
         <div className="container">
+          <div class="input-group">
+            <div class="form-outline">
+              <input
+                placeholder="Search..."
+                type="search"
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+                id="form1"
+                class="form-control"
+              />
+            </div>
+          </div>
           <div className="row align-items-end">
             <div className="col-xxl-5 col-xl-6 col-lg-6">
               <div className="section__title-wrapper mb-60">
@@ -102,9 +116,17 @@ export default () => {
           </div>
           <TabPanel>
             <div className="row">
-              {course.map((item) => (
-                <CourseCard item={item} />
-              ))}
+              {course.map((item) => {
+                if (searchText) {
+                  return (
+                    item?.name?.toLowerCase().startsWith(searchText) && (
+                      <CourseCard item={item} />
+                    )
+                  );
+                } else {
+                  return <CourseCard item={item} />;
+                }
+              })}
             </div>
             <div className="d-flex justify-content-center">
               <ResponsivePagination
