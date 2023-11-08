2d6e8e9c983885fc12afa310fa57c326470202bd
rahul <rahulor2000@gmail.com>
2023-11-08 13:33:02 +0530

    some page updated

--- src/components/Home/DashTest.js
+++ src/components/Home/DashTest.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import Link from 'next/link';
-import DashboardBar from '../Sidebar/DashboardBar';
-import Accordion from 'react-bootstrap/Accordion';
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+import React, { Component } from "react";
+import Link from "next/link";
+import DashboardBar from "../Sidebar/DashboardBar";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 import fetchData from "../../axios";
 import DataTable from "react-data-table-component";
 
@@ -31,7 +31,6 @@ const customStyles = {
 };
 
 class DashTest extends Component {
-
   constructor() {
     super();
     this.state = {
@@ -54,7 +53,7 @@ class DashTest extends Component {
   getData = () => {
     try {
       const makeRequest = fetchData();
-      makeRequest("GET","/course/get-bought-course")
+      makeRequest("GET", "/course/get-bought-course")
         .then((res) => {
           console.log(res);
           this.setState({
@@ -70,50 +69,49 @@ class DashTest extends Component {
     }
   };
 
-   
-      render() {
-        const columns = [
-          {
-            name: "No",
-            selector: (row,idx) => idx+1,
-            sortable: true,
-          },
-          {
-            name: "Name",
-            selector: (row) => row.Name,
-            sortable: true,
-          },
-          {
-            name: "description",
-            selector: (row) => row.description.slice(0,25),
-          },
-          {
-            name: "category",
-            selector: (row) => row.category,
-          },
-          {
-            name: "validity",
-            selector: (row) => {
-              let date = row.validity.split("/")
-              let newDate = `${date[1]}/${date[0]}/${date[2]}`
-              return newDate
-            },
-          },
-          {
-            name: "",
-            cell: () => (
-              <a href={"#"} className="btn btn-success">
-                Start
-              </a>
-            ),
-          },
-        ];
+  render() {
+    const columns = [
+      {
+        name: "No",
+        selector: (row, idx) => idx + 1,
+        sortable: true,
+      },
+      {
+        name: "Name",
+        selector: (row) => row.Name,
+        sortable: true,
+      },
+      {
+        name: "description",
+        selector: (row) => row.description.slice(0, 25),
+      },
+      {
+        name: "category",
+        selector: (row) => row.category,
+      },
+      {
+        name: "validity",
+        selector: (row) => {
+          let date = row.validity
+            .split("/")
+            .map((d) => (d.length <= 1 ? "0" + d : d));
+          let newDate = `${date[1]}/${date[0]}/${date[2]}`;
+          return newDate;
+        },
+      },
+      {
+        name: "",
+        cell: () => (
+          <a href={"#"} className="btn btn-success">
+            Start
+          </a>
+        ),
+      },
+    ];
 
-        return (
-
-         <div className='container' style={{padding:'10px'}}>
-
-              {/* <div>  
+    return (
+      <div className="container" style={{ padding: "10px" }}>
+        {/* <div>  
                 <Card style={{ width: '18rem', marginTop:'50px'}}>
       <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
       <Card.Body>
@@ -134,119 +132,114 @@ class DashTest extends Component {
     </Card>
     </div>    */}
 
-           
+        <div className="ag-format-container">
+          <h2
+            style={{
+              padding: "1.5rem",
+              color: "#212450",
+              display: "flex",
+              justifyContent: "flex-start",
+              justifyContent: "center",
+              marginTop: "20px",
+              fontSize: 46,
+            }}
+          >
+            Dashboard
+          </h2>
+          <div className="ag-courses_box dash-shadow">
+            <div className="ag-courses_item">
+              <a href="/company/myprofile" className="ag-courses-item_link">
+                <div className="ag-courses-item_bg"></div>
 
-         <div className="ag-format-container">
-         <h2 style={{padding:"1.5rem", color: "#212450", display:"flex", justifyContent:"flex-start",justifyContent:"center", marginTop:'20px',fontSize: 46}}>Dashboard</h2>
-  <div className="ag-courses_box dash-shadow">
-   
-    <div className="ag-courses_item">
-      <a href="/company/myprofile" className="ag-courses-item_link">
-        <div className="ag-courses-item_bg"></div>
+                <div className="ag-courses-item_title">My Profile</div>
 
-        <div className="ag-courses-item_title">
-        My Profile
-        </div>
-
-        <div className="bi bi-person-circle ag-courses-item_date-box">
-          {/* Start:
+                <div className="bi bi-person-circle ag-courses-item_date-box">
+                  {/* Start:
           <span className="ag-courses-item_date">
             04.11.2022
           </span> */}
-        </div>
-      </a>
-    </div>
+                </div>
+              </a>
+            </div>
 
-    <div className="ag-courses_item">
-      <a href="/company/mycourses" className="ag-courses-item_link">
-        <div className="ag-courses-item_bg"></div>
+            <div className="ag-courses_item">
+              <a href="/company/mycourses" className="ag-courses-item_link">
+                <div className="ag-courses-item_bg"></div>
 
-        <div className="ag-courses-item_title">
-        My Course
-        </div>
+                <div className="ag-courses-item_title">My Course</div>
 
-        <div className="bi bi-archive ag-courses-item_date-box">
-          {/* Start:
+                <div className="bi bi-archive ag-courses-item_date-box">
+                  {/* Start:
           <span className="ag-courses-item_date">
             04.11.2022
           </span> */}
-        </div>
-      </a>
-    </div>
+                </div>
+              </a>
+            </div>
 
-    
+            <div className="ag-courses_item">
+              <a href="#" className="ag-courses-item_link">
+                <div className="ag-courses-item_bg"></div>
 
-    <div className="ag-courses_item">
-      <a href="#" className="ag-courses-item_link">
-        <div className="ag-courses-item_bg"></div>
+                <div className="ag-courses-item_title">Training Matrix</div>
 
-        <div className="ag-courses-item_title">
-          Training Matrix
-        </div>
-
-        <div className="bi bi-file-earmark-spreadsheet ag-courses-item_date-box">
-          {/* Start:
+                <div className="bi bi-file-earmark-spreadsheet ag-courses-item_date-box">
+                  {/* Start:
           <span className="ag-courses-item_date">
             04.11.202
           </span> */}
-        </div>
-      </a>
-    </div>
-
-
-
-  </div>
-  <div className="dash-shadow">
-        <h4
-          style={{
-            padding: "1rem",
-            color: "#212450",
-            display: "flex",
-            justifyContent: "flex-start",
-            justifyContent: "center",
-            marginTop: "2.5rem",
-            fontSize: 35,
-          }}
-        >
-          Courses to do
-        </h4>
-        <div className=" row g-3  min-vh-100  d-flex justify-content-center ">
-          <div style={{ padding: "", backgroundColor: "" }}>
-            <div
-              className="pb-2 smth"
-              style={{ display: "flex", justifyContent: "right" }}
-            >
-              <input
-                type="text"
-                className=""
-                placeholder="Search course..."
-                onChange={this.handleFilter}
-                style={{
-                  padding: "6px 10px",
-                  borderColor: "gray",
-                  overflow: "hidden",
-                  marginRight:'.2rem'
-                }}
-              />
+                </div>
+              </a>
             </div>
-            <div style={{padding:'.2rem'}}>
-            <DataTable 
-              columns={columns}
-              data={this.state.records}
-              customStyles={customStyles}
-              pagination
-              selectableRows
-            /></div>
+          </div>
+          <div className="dash-shadow">
+            <h4
+              style={{
+                padding: "1rem",
+                color: "#212450",
+                display: "flex",
+                justifyContent: "flex-start",
+                justifyContent: "center",
+                marginTop: "2.5rem",
+                fontSize: 35,
+              }}
+            >
+              Courses to do
+            </h4>
+            <div className=" row g-3  min-vh-100  d-flex justify-content-center ">
+              <div style={{ padding: "", backgroundColor: "" }}>
+                <div
+                  className="pb-2 smth"
+                  style={{ display: "flex", justifyContent: "right" }}
+                >
+                  <input
+                    type="text"
+                    className=""
+                    placeholder="Search course..."
+                    onChange={this.handleFilter}
+                    style={{
+                      padding: "6px 10px",
+                      borderColor: "gray",
+                      overflow: "hidden",
+                      marginRight: ".2rem",
+                    }}
+                  />
+                </div>
+                <div style={{ padding: ".2rem" }}>
+                  <DataTable
+                    columns={columns}
+                    data={this.state.records}
+                    customStyles={customStyles}
+                    pagination
+                    selectableRows
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-  
-</div>
-
-          
-
-           </div>
-        )       
-        }
-    }
-export default DashTest
+    );
+  }
+}
+export default DashTest;
--- src/pages/company/dashboard.js
+++ src/pages/company/dashboard.js
@@ -4,37 +4,47 @@ import DashboardBar from "../../components/Sidebar/DashboardBar";
 import HeaderDashboard from "../../components/Layout/Header/HeaderDashboard";
 import DashMain from "../../components/Home/DashMain";
 import DashCourse from "../../components/Home/DashCourse";
-import Header from '../../components/Layout/Header/Header';
-import NoSSR from 'react-no-ssr';
-import DashTest from '../../components/Home/DashTest';
+import Header from "../../components/Layout/Header/Header";
+import NoSSR from "react-no-ssr";
+import DashTest from "../../components/Home/DashTest";
 
-class dashboard extends React.Component{
-
-    static getInitialProps({store}) {}
-    constructor(props) {
-        super(props);
-    }
-    render(){    
-        return(
-            <React.Fragment>
-                <main  className="p-4" style={{backgroundImage: "linear-gradient(to right, #EDEEF3, #EDEEF3)" }}>
-                <NoSSR>
-                 <Header/>
-                 </NoSSR>
-                 <div className="container-fluid " style={{borderRadius:'22px',marginTop:"120px"}}> 
-                  <div className="row justify-content-md-center">
-                   <div className="col-sm-2 col-md-auto " style={{backgroundColor:'#212450'}}>
-                   <DashboardBar/>
-                   </div>
-                <div className="col-sm col-md-9  bg-white">
-                    <DashTest />
-             </div>
-         </div>       
-      </div>
-    </main>
-  </React.Fragment>
-        )
-    }
+class dashboard extends React.Component {
+  static getInitialProps({ store }) {}
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <main
+          className="p-4"
+          style={{
+            backgroundImage: "linear-gradient(to right, #EDEEF3, #EDEEF3)",
+          }}
+        >
+          <NoSSR>
+            <Header />
+          </NoSSR>
+          <div
+            className="container-fluid "
+            style={{ borderRadius: "22px", marginTop: "120px" }}
+          >
+            <div className="row justify-content-md-center">
+              <div
+                className="col-sm-2 col-md-auto "
+                style={{ backgroundColor: "#212450" }}
+              >
+                <DashboardBar />
+              </div>
+              <div className="col-sm col-md-9  bg-white">
+                <DashTest />
+              </div>
+            </div>
+          </div>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
-export default dashboard
+export default dashboard;
