0679e7339a8b26650b09e9dfce3d4d2bfb9c89df
Kiran Johnson <mekiranjohnson@gmail.com>
2023-11-10 11:33:39 +0530

    search removed

--- src/components/Elements/Tab/CourseGridAll.js
+++ src/components/Elements/Tab/CourseGridAll.js
@@ -76,24 +76,17 @@ export default () => {
     <section className="course__area pt-50 pb-60 grey-bg">
       <Tabs variant="enclosed" id="react-tabs-276">
         <div className="container">
-          <div className="row">
+          <div className="row" >
             <div className="form-outline col-3">
-              <input
-                placeholder="Search..."
-                type="search"
-                value={searchText}
-                onChange={(e) => setSearchText(e.target.value)}
-                id="form1"
-                className="form-control"
-              />
+         
             </div>
-            <div className="col-3">
-              <select
-                className="form-control"
+            <div  className="col-3">
+              <select style={{background:'#edeef3',paddingBottom:"1rem"}}
+                className="form-control text-secondary "
                 aria-label="Default select example"
                 onChange={(e) => setCategoryFilter(e.target.value)}
               >
-                <option value="">select category</option>
+                <option  value="">filter</option>
                 <option value="Care Certificate">Care Certificate</option>
                 <option value="Mandatory Care Courses">
                   Mandatory Care Courses
