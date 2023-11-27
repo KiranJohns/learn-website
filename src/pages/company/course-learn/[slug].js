0a5bbcd102a43478ccb93cce27e64d52a1440f75
Kiran Johnson <mekiranjohnson@gmail.com>
2023-11-27 21:13:37 +0530

    error solve2

--- src/pages/company/course-learn/[slug].js
+++ src/pages/company/course-learn/[slug].js
@@ -1,38 +1,38 @@
-import React, { useEffect } from "react";
-import DashboardBar from "../../../components/Sidebar/DashboardBar";
-import Header from "../../../components/Layout/Header/Header";
-import NoSSR from "react-no-ssr";
-import { useRouter } from "next/router";
-import SingleCourse from "../../../components/CourseDetails/SingleCourse";
-import fetchData from "../../../axios";
-import { useState } from "react";
+// import React, { useEffect } from "react";
+// import DashboardBar from "../../../components/Sidebar/DashboardBar";
+// import Header from "../../../components/Layout/Header/Header";
+// import NoSSR from "react-no-ssr";
+// import { useRouter } from "next/router";
+// import SingleCourse from "../../../components/CourseDetails/SingleCourse";
+// import fetchData from "../../../axios";
+// import { useState } from "react";
 
-function CourseDetails() {
-  return (
-    <main
-      className="p-4"
-      style={{ backgroundImage: "linear-gradient(to right, #EDEEF3, #EDEEF3)" }}
-    >
-      <NoSSR>
-        <Header />
-      </NoSSR>
-      <div
-        className="container-fluid "
-        style={{ borderRadius: "22px", marginTop: "120px" }}
-      >
-        <div className="row justify-content-md-center">
-          <div className="col-sm-12 col-md-12 col-lg-2 p-0" style={{ backgroundColor: "#212450" }}>
-            <DashboardBar />
-          </div>
-          <div className="col-sm col-md-9 bg-white">
+// function CourseDetails() {
+//   return (
+//     <main
+//       className="p-4"
+//       style={{ backgroundImage: "linear-gradient(to right, #EDEEF3, #EDEEF3)" }}
+//     >
+//       <NoSSR>
+//         <Header />
+//       </NoSSR>
+//       <div
+//         className="container-fluid "
+//         style={{ borderRadius: "22px", marginTop: "120px" }}
+//       >
+//         <div className="row justify-content-md-center">
+//           <div className="col-sm-12 col-md-12 col-lg-2 p-0" style={{ backgroundColor: "#212450" }}>
+//             <DashboardBar />
+//           </div>
+//           <div className="col-sm col-md-9 bg-white">
       
-            <SingleCourse  />
+//             <SingleCourse  />
           
-          </div>
-        </div>
-      </div>
-    </main>
-  );
-}
+//           </div>
+//         </div>
+//       </div>
+//     </main>
+//   );
+// }
 
-export default CourseDetails;
+// export default CourseDetails;
