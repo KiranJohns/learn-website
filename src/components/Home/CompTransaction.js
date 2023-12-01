6581b7737fba4f5914347871e5c5794f0f52c4bc
Kiran Johnson <mekiranjohnson@gmail.com>
2023-12-02 00:08:15 +0530

    report

--- src/components/Home/CompTransaction.js
+++ src/components/Home/CompTransaction.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import axios from "axios";
 import DataTable from "react-data-table-component";
+import Link from "next/link";
 import BasicExample from "../About/button1";
 import fetchData from "../../axios";
 import Button from 'react-bootstrap/Button';
@@ -26,103 +27,129 @@ const customStyles = {
   },
 };
 
-const CompTransaction = () => {
-  const [records, setRecords] = useState([]);
-  const [filterRecords, setFilterRecords] = useState([]);
-
-  const handleFilter = (event) => {
-    const newData = filterRecords.filter((row) =>
-      row.name.toLowerCase().includes(event.target.value.toLowerCase())
-    );
-    setRecords(newData);
-  };
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        let makeRequest = fetchData();
-        const res = await makeRequest("GET", "/info/get-all-transactions");
-        console.log(res);
-        setRecords(res.data.response);
-        setFilterRecords(res.data);
-      } catch (err) {
-        console.log(err);
-      }
+class CompTransaction extends Component {
+  constructor() {
+    super();
+    this.state = {
+      records: [],
+      filterRecords: [],
     };
+  }
+  
+  handleFilter = (event) => {
+    const newData = this.state.filterRecords.filter((row) =>
+    row.name.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    this.setState({ records: newData });
+  };
+  
+  componentDidMount() {
+    console.clear()
+    let makeRequest = fetchData();
+    makeRequest("GET", "/info/get-all-transactions")
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          records: res.data.response.reverse(),
+          filterRecords: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    getData();
-  }, []);
+  render() {
+    const columns = [
+      {
+        name: "NO",
+        selector: (row,idx) => ++idx,
+        center: true,
+      },
+      {
+        name: "Date",
+        selector: (row) => new Date(row.date).toLocaleDateString(),
+        center: true,
+        sortable: true,
+      },
+      {
+        name: "Time",
+        selector: (row) => new Date(row.date).toLocaleTimeString('en-US'),
+        center: true,
+        sortable: true,
+      },
+      {
+        name: "Quantity",
+        selector: (row) => row.count,
+        center: true,
+      },
+      {
+        name: "Amount",
+        selector: (row) => row.amount,
+        center: true,
+      },
+    ];
 
-  const columns = [
-    {
-      name: "Sl",
-      selector: (row, idx) => ++idx,
-      sortable: true,
-    },
-    {
-      name: "Date",
-      selector: (row) => row.bundle_name,
-      sortable: true,
-    },
-    {
-      name: "Time",
-      selector: (row) => row.bundle_name,
-      sortable: true,
-    },
-    {
-      name: "Quantity",
-      selector: (row) => row.course_count,
-    },
-    {
-      name: "Amount",
-      selector: (row) => row.amount,
-    },
-  ];
-
-  return (
-    <div className="">
+    return (
+      <div className="">
+       
       <div className="dash-shadow">
-        <div className="row g-3 min-vh-100 d-flex justify-content-center mt-20">
-          <h2
-            style={{
-              color: "#212450",
-              display: "flex",
-              justifyContent: "center",
-              position: 'absolute',
-              fontSize: 38,
-            }}
+      <div className=" row g-3  min-vh-100  d-flex justify-content-center mt-20">
+      <h2
+        style={{  
+          color: "#212450",
+          display: "flex",
+          justifyContent: "center",
+          position:'absolute',
+          fontSize: 38,
+        }}
+      >
+      Transactions
+      </h2>
+        <div style={{ padding: "", backgroundColor: "" }}>
+          {/* <div
+            className="pb-2 smth"
+            style={{ display: "flex", justifyContent: "left" }}
           >
-            Transactions
-          </h2>
-          <div style={{ padding: "", backgroundColor: "" }}>
-            <div style={{ float: 'right', marginBottom: '1.4rem' }} className="p-relative d-inline header__search">
-              <form action="">
-                <input
-                  style={{ background: '#edeef3', }}
-                  className="d-block mr-10"
-                  type="text"
-                  placeholder="Search..."
-                  // value={searchString}
-                  // onChange={handleSearch}
-                />
-                <button type="submit">
-                  <i className="fas fa-search"></i>
-                </button>
-              </form>
-            </div>
-            <DataTable
-              persistTableHead={true}
-              columns={columns}
-              data={records}
-              customStyles={customStyles}
-              pagination
-              selectableRows
+            <input
+              type="text"
+              className=""
+              placeholder="Search course..."
+              onChange={this.handleFilter}
+              style={{
+                padding: "6px 10px",
+                borderColor: "transparent",
+                overflow: "hidden",
+              }}
             />
+          </div> */}
+          <div style={{float:'right',marginBottom:'1.4rem'}} className="p-relative d-inline header__search">
+            <form action="">
+              <input style={{ background:'#edeef3',}}
+                className="d-block mr-10"
+                type="text"
+                placeholder="Search..."
+                // value={searchString}
+                // onChange={handleSearch}
+              />
+              <button type="submit">
+                <i className="fas fa-search"></i>
+              </button>
+            </form>
           </div>
+          <DataTable
+          persistTableHead={true}
+            columns={columns}
+            data={this.state.records}
+            customStyles={customStyles}
+            pagination
+            selectableRows
+          />
         </div>
-      </div>
+      </div> </div>
     </div>
-  );
-};
+    );
+  }
+}
 
 export default CompTransaction;
