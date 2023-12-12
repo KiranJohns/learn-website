fec04480fb67dd3cffd7168fde257362bf89c5fe
Kiran Johnson <mekiranjohnson@gmail.com>
2023-12-12 20:00:53 +0530

    error

--- src/pages/success.js
+++ src/pages/success.js
@@ -3,6 +3,7 @@ import Header from "../components/Layout/Header/Header";
 import Footer from "../components/Layout/Footer/Footer";
 import HeaderSuccess from "../components/Layout/Header/HeaderSuccess";
 import SucessLayout from "../components/Common/sucessLayout";
+import NoSSR from "react-no-ssr";
 
 class Success extends React.Component {
   static getInitialProps({ store }) {}
