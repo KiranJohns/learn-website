afdd1d8885a758e5406349ac7635f6a14b0fb0b9
Kiran Johnson <mekiranjohnson@gmail.com>
2023-12-12 20:11:04 +0530

    error

--- src/pages/success.js
+++ src/pages/success.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Layout/Header/Header";
 import Footer from "../components/Layout/Footer/Footer";
 import HeaderSuccess from "../components/Layout/Header/HeaderSuccess";
+import HeaderOpaque from "../components/Layout/Header/HeaderOpaque";
 import SucessLayout from "../components/Common/sucessLayout";
 import NoSSR from "react-no-ssr";
 
@@ -18,6 +19,17 @@ class Success extends React.Component {
         <NoSSR>
           <HeaderSuccess />
         </NoSSR>
+        <NoSSR>
+          <div style={{visibility:"hidden"}}>  
+             <HeaderOpaque />
+          </div>
+        </NoSSR>
+        <NoSSR>
+          <div style={{visibility:"hidden"}}>
+          <Header />
+          </div>
+        </NoSSR>
+
         <SucessLayout />
       </React.Fragment>
     );
