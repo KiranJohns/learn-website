eedd15c85a737d5bd02b59c9745bcb3ad2d49a62
Kiran Johnson <mekiranjohnson@gmail.com>
2023-12-14 20:34:28 +0530

    success page

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,45 +1,38 @@
 import React from "react";
 import Header from "../components/Layout/Header/Header";
-import Footer from "../components/Layout/Footer/Footer";
 import HeaderSuccess from "../components/Layout/Header/HeaderSuccess";
 import HeaderOpaque from "../components/Layout/Header/HeaderOpaque";
-import SucessLayout from "../components/Common/sucessLayout";
+import SucessLayout from "../components/Common/successLayout";
 import NoSSR from "react-no-ssr";
 import ShopingCart from "../components/Layout/Header/ShopingCart";
 
-class Success extends React.Component {
-  static getInitialProps({ store }) {}
+const Success = () => {
+  return (
+    <>
+      <NoSSR>
+        <HeaderOpaque />
+      </NoSSR>
+      <NoSSR>
+        <div style={{ visibility: "hidden" }}>
+          <HeaderSuccess />
+        </div>
+      </NoSSR>
+      <NoSSR>
+        <div style={{ visibility: "hidden" }}>
+          <Header />
+        </div>
+      </NoSSR>
+      <NoSSR>
+        <div style={{ visibility: "hidden" }}>
+          <ShopingCart />
+        </div>
+      </NoSSR>
 
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <NoSSR>
-          <HeaderOpaque />
-        </NoSSR>
-        <NoSSR>
-          <div style={{ visibility: "hidden" }}> <HeaderSuccess/></div>  
-        </NoSSR>
-        <NoSSR>
-          <div style={{ visibility: "hidden" }}>
-            <Header />
-          </div>
-        </NoSSR>
-        <NoSSR>
-          <div style={{ visibility: "hidden" }}>
-            <ShopingCart />
-          </div>
-        </NoSSR>
-
-        <NoSSR>
-          <SucessLayout />
-        </NoSSR>
-      </React.Fragment>
-    );
-  }
-}
+      <NoSSR>
+        <SucessLayout />
+      </NoSSR>
+    </>
+  );
+};
 
 export default Success;
