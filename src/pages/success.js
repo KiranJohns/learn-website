a4f5cebb00088841f2bc3cd4514de9e20f04fd92
Kiran Johnson <mekiranjohnson@gmail.com>
2023-12-14 20:28:30 +0530

    success page

--- src/pages/success.js
+++ src/pages/success.js
@@ -3,37 +3,43 @@ import Header from "../components/Layout/Header/Header";
 import Footer from "../components/Layout/Footer/Footer";
 import HeaderSuccess from "../components/Layout/Header/HeaderSuccess";
 import HeaderOpaque from "../components/Layout/Header/HeaderOpaque";
-import SucessLayout from "../components/Common/successLayout";
+import SucessLayout from "../components/Common/sucessLayout";
 import NoSSR from "react-no-ssr";
 import ShopingCart from "../components/Layout/Header/ShopingCart";
 
-const Success = () => {
-  return (
-    <React.Fragment>
-      <NoSSR>
-        <HeaderOpaque />
-      </NoSSR>
-      <NoSSR>
-        <div style={{ visibility: "hidden" }}>
-          <HeaderSuccess />
-        </div>
-      </NoSSR>
-      <NoSSR>
-        <div style={{ visibility: "hidden" }}>
-          <Header />
-        </div>
-      </NoSSR>
-      <NoSSR>
-        <div style={{ visibility: "hidden" }}>
-          <ShopingCart />
-        </div>
-      </NoSSR>
+class Success extends React.Component {
+  static getInitialProps({ store }) {}
 
-      <NoSSR>
-        <SucessLayout />
-      </NoSSR>
-    </React.Fragment>
-  );
-};
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <NoSSR>
+          <HeaderOpaque />
+        </NoSSR>
+        <NoSSR>
+          <div style={{ visibility: "hidden" }}> <HeaderSuccess/></div>  
+        </NoSSR>
+        <NoSSR>
+          <div style={{ visibility: "hidden" }}>
+            <Header />
+          </div>
+        </NoSSR>
+        <NoSSR>
+          <div style={{ visibility: "hidden" }}>
+            <ShopingCart />
+          </div>
+        </NoSSR>
+
+        <NoSSR>
+          <SucessLayout />
+        </NoSSR>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Success;
