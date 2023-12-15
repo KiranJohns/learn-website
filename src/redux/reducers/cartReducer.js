d01e6303792413d7892f9a480a2e36f8268ccf7a
rahul <rahulor2000@gmail.com>
2023-12-15 07:30:22 +0530

    cart delete function upddated
    nan error fixed

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -125,7 +125,9 @@ const cartReducer = function (state = initialUserState, action) {
       let product_count = 0;
       state.cart = state.cart.filter((item) => {
         if (item.id == action.payload) {
-          state.totalPrice -= Number(item.price) * Number(item.product_count);
+          console.log(item);
+          console.log(item.price,item.product_count);
+          state.totalPrice -= Number(item.amount);
           product_count = item.product_count;
           return null;
         }
