de0087e0378b3586e73c00c668f92dbdd40678ac
Kiran Johnson <mekiranjohnson@gmail.com>
2023-12-16 10:30:19 +0530

    cart responsive

--- src/components/MyCart/MyCartMain.js
+++ src/components/MyCart/MyCartMain.js
@@ -96,7 +96,7 @@ const MyCart = () => {
         } else {
           let per = parseFloat(
             (parseFloat(totalPrice) * parseFloat(res.data.response.amount)) /
-              100
+            100
           );
           setOfferPrice(parseFloat(parseInt(totalPrice) - per).toFixed(2));
         }
@@ -172,12 +172,12 @@ const MyCart = () => {
       <Breadcrumb pageTitle="My Cart" />
       {/* breadcrumb-end */}
 
-      <section className="cart-area pt-100 pb-100">
+      <section className="cart-area pt-70 pb-100">
         <div className="container">
           <div className="row">
             <div className="col-12">
               <div className="table-content table-responsive">
-                <table className="table">
+                <table className="table  cart-table-display">
                   <thead>
                     <tr>
                       <th className="product-thumbnail">Course</th>
@@ -190,9 +190,9 @@ const MyCart = () => {
                   </thead>
                   <tbody>
                     {cart &&
-                      cart.map((item) => {
+                      cart.map((item, id) => {
                         return (
-                          <tr>
+                          <tr key={{ id }}>
                             <td className="product-thumbnail">
                               <Link href={`/course/${item.course_id}`}>
                                 <a>
@@ -277,19 +277,100 @@ const MyCart = () => {
                   </tbody>
                 </table>
 
-                <div style={{display:'none'}} className="row">
-                  <div className="col-12">
-                  {cart &&
-                      cart.map((item) => {
+                <div className="cart-card-display" style={{ marginTop: "1rem", padding: "1rem" }}>
+                  <div style={{display:"flex", justifyContent:"center"}} className="row">
+                    {/* <div style={{display:'flex'}} className="col-12"> */}
+                 
+                    {cart &&
+                      cart.map((item, id) => {
                         return (
-                       <div  className="dash-shadow">
-                        <div >
-                          <img style={{height:"10rem", width:"13rem"}} src={item.thumbnail} alt="" />
-                        </div>
-                       </div>
-                        )})}
+                          <div key={{ id }} style={{ width: "28em", height: "20rem", position: "relative" }} className="cart-card-shadow col-10 ">
+
+                            <div style={{ position: 'absolute', right: "0", padding: ".5rem" }}>
+                              <span onClick={() => removeItem(item.id)}>
+                                <FaTrash
+                                  style={{
+                                    fontSize: "1rem",
+                                    cursor: "pointer",
+                                    color: "#212a50"
+                                  }}
+                                />
+                              </span>
+                            </div>
+
+                            <div style={{ display: 'flex',justifyContent:"space-evenly", alignItems:'center' }}>
+                              <div style={{ padding: '1rem' }}>
+                                <Link href={`/course/${item.course_id}`}>
+                                  <img style={{ height: "8rem", width: "13rem" }} src={item.thumbnail} alt="" />
+                                </Link>
+                              </div>
+
+
+                              <div style={{ marginTop: "2.5rem" }}>
+                                <h4 className="unit-font-s" style={{ color: "#212a50" }}>Unit Price</h4>
+                                <div>
+                                  <span style={{ textAlign: 'center' }} className="amount"><p>£{parseFloat(item.amount / item.product_count).toFixed(2)}</p></span></div>
+                              </div>
+                            </div>
+
+                            <div style={{display:"flex", justifyContent:'center'}}> 
+                               <div style={{ }}><h4 style={{ color: '#212a50', marginTop: '1rem' }}>  <Link href={`/course/${item.course_id}`}>
+                              <a >{item.name}</a>
+                            </Link></h4>
+                              <div>
+                                {/* <strong>Quantity: {item.quantity}</strong> */}
+                                <div className="product-quantity-form d-flex flex-column align-items-center">
+                                  <button
+                                    style={{
+                                      cursor: "pointer",
+                                    }}
+                                    className="cart-minus"
+                                    onClick={() =>
+                                      decrement(
+                                        item.course_id,
+                                        item.id,
+                                        item.item_type
+                                      )
+                                    }
+                                  >
+                                    <i className="fas fa-minus"></i>
+                                  </button>
+                                  <p
+                                    style={{
+                                      padding: "0.8rem",
+                                      marginTop: "0.4rem",
+                                    }}
+                                  >
+                                    {item.product_count}
+                                  </p>
+                                  {/* <p>{this.state.num}</p> */}
+                                  <button
+                                    style={{
+                                      cursor: "pointer",
+                                    }}
+                                    className="cart-plus"
+                                    onClick={() =>
+                                      increment(
+                                        item.course_id,
+                                        item.id,
+                                        item.item_type
+                                      )
+                                    }
+                                  >
+                                    <i className="fas fa-plus"></i>
+                                  </button>
+                                </div>
+                              </div>
+                            </div>
+                            </div>
+
+
+                          </div>
+                        )
+                      })}
+                    {/* </div> */}
                   </div>
-            </div>
+                </div>
 
               </div>
               <div className="row">
@@ -375,7 +456,8 @@ const MyCart = () => {
                         Subtotal
                         <h4>
                           <span
-                            style={{ color: "#212a50", fontSize: "1.2rem" }}
+                           className="discount-font"
+                            style={{ color: "#212a50",  }}
                           >
                             £ {parseFloat(totalPrice).toFixed(2)}
                           </span>
@@ -390,7 +472,8 @@ const MyCart = () => {
                         Discount
                         <h4>
                           <span
-                            style={{ color: "#212a50", fontSize: "1.2rem" }}
+                          className="discount-font"
+                            style={{ color: "#212a50", }}
                           >
                             £{" "}
                             {offerPrice
@@ -409,6 +492,7 @@ const MyCart = () => {
                         Grand Total
                         <h4>
                           <span
+                          className="grand-total-f"
                             style={{ color: "#212a50", fontSize: "1.2rem" }}
                           >
                             £ {offerPrice ? parseFloat(offerPrice).toFixed(2) : parseFloat(totalPrice).toFixed(2)}
