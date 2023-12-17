4e28fdaa5292f2f916dc8dfa0412d773b25fe15b
rahul <rahulor2000@gmail.com>
2023-12-17 17:18:27 +0530

    individual transaction table search updated

--- src/components/Home/IndTransaction.js
+++ src/components/Home/IndTransaction.js
@@ -4,7 +4,7 @@ import DataTable from "react-data-table-component";
 import Link from "next/link";
 import BasicExample from "../About/button1";
 import fetchData from "../../axios";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import Spinner from "react-bootstrap/Spinner";
 
 const customStyles = {
@@ -34,18 +34,19 @@ class Transaction extends Component {
     this.state = {
       records: [],
       filterRecords: [],
+      searchString: "",
     };
   }
-  
+
   handleFilter = (event) => {
     const newData = this.state.filterRecords.filter((row) =>
-    row.name.toLowerCase().includes(event.target.value.toLowerCase())
+      row.name.toLowerCase().includes(event.target.value.toLowerCase())
     );
     this.setState({ records: newData });
   };
-  
+
   componentDidMount() {
-    console.clear()
+    console.clear();
     let makeRequest = fetchData();
     makeRequest("GET", "/info/get-all-transactions")
       .then((res) => {
@@ -64,14 +65,14 @@ class Transaction extends Component {
     const columns = [
       {
         name: "SL",
-        selector: (row,idx) => ++idx,
-        width:'100px',
+        selector: (row, idx) => ++idx,
+        width: "100px",
         center: true,
       },
       {
         name: "User",
-        selector: (row,idx) => row.first_name.concat(" ",row.last_name),
-        width:'100px',
+        selector: (row, idx) => row.first_name.concat(" ", row.last_name),
+        width: "100px",
         center: true,
       },
       {
@@ -82,7 +83,7 @@ class Transaction extends Component {
       },
       {
         name: "Time",
-        selector: (row) => row.time, 
+        selector: (row) => row.time,
         center: true,
         sortable: true,
       },
@@ -100,22 +101,21 @@ class Transaction extends Component {
 
     return (
       <div className="">
-       
-      <div className="dash-shadow">
-      <div className=" row g-3  min-vh-100  d-flex justify-content-center mt-20">
-      <h2
-        style={{  
-          color: "#212450",
-          display: "flex",
-          justifyContent: "center",
-          position:'absolute',
-          fontSize: 38,
-        }}
-      >
-      Transactions
-      </h2>
-        <div style={{ padding: "", backgroundColor: "" }}>
-          {/* <div
+        <div className="dash-shadow">
+          <div className=" row g-3  min-vh-100  d-flex justify-content-center mt-20">
+            <h2
+              style={{
+                color: "#212450",
+                display: "flex",
+                justifyContent: "center",
+                position: "absolute",
+                fontSize: 38,
+              }}
+            >
+              Transactions
+            </h2>
+            <div style={{ padding: "", backgroundColor: "" }}>
+              {/* <div
             className="pb-2 smth"
             style={{ display: "flex", justifyContent: "left" }}
           >
@@ -131,31 +131,49 @@ class Transaction extends Component {
               }}
             />
           </div> */}
-          <div style={{float:'right',marginBottom:'1.4rem'}} className="p-relative d-inline header__search">
-            <form action="">
-              <input style={{ background:'#edeef3',}}
-                className="d-block mr-10"
-                type="text"
-                placeholder="Search..."
-                // value={searchString}
-                // onChange={handleSearch}
+              <div
+                style={{ float: "right", marginBottom: "1.4rem" }}
+                className="p-relative d-inline header__search"
+              >
+                <form action="">
+                  <input
+                    style={{ background: "#edeef3" }}
+                    className="d-block mr-10"
+                    type="text"
+                    placeholder="Search..."
+                    value={this.state.searchString}
+                    onChange={(e) =>
+                      this.setState({
+                        ...this.state,
+                        searchString: e.target.value,
+                      })
+                    }
+                  />
+                  <button type="submit">
+                    <i className="fas fa-search"></i>
+                  </button>
+                </form>
+              </div>
+              <DataTable
+                noDataComponent={" "}
+                persistTableHead={true}
+                columns={columns}
+                data={
+                  this.state.searchString
+                    ? this.state.records.filter((item) =>
+                        item.first_name
+                          .toLowerCase()
+                          .startsWith(this.state.searchString.toLowerCase())
+                      )
+                    : this.state.records
+                }
+                customStyles={customStyles}
+                pagination
               />
-              <button type="submit">
-                <i className="fas fa-search"></i>
-              </button>
-            </form>
-          </div>
-          <DataTable
-            noDataComponent={" "}
-          persistTableHead={true}
-            columns={columns}
-            data={this.state.records}
-            customStyles={customStyles}
-            pagination
-          />
+            </div>
+          </div>{" "}
         </div>
-      </div> </div>
-    </div>
+      </div>
     );
   }
 }
