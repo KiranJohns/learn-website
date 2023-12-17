642bca806bf49374107c4c05b2bc774e1e18e732
rahul <rahulor2000@gmail.com>
2023-12-17 12:43:40 +0530

    company individuals-report table search updated

--- src/components/Home/CompIndiviRep.js
+++ src/components/Home/CompIndiviRep.js
@@ -147,7 +147,7 @@ const CompIndReport = () => {
                 data={
                   searchString
                     ? records.filter((item) =>
-                        item.name.toLowerCase().includes(searchString.toLowerCase())
+                        item.first_name.toLowerCase().startsWith(searchString.toLowerCase())
                       )
                     : records
                 }
