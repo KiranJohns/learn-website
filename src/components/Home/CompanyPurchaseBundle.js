3c9643894424bf80454320f16d02a257a823fdbe
Kiran Johnson <mekiranjohnson@gmail.com>
2023-12-23 19:38:46 +0530

    no data comp

--- src/components/Home/CompanyPurchaseBundle.js
+++ src/components/Home/CompanyPurchaseBundle.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 import DataTable from "react-data-table-component";
-import Link from "next/link";
-import BasicExample from "../About/button1";
-import fetchData from "../../axios";
 import Button from 'react-bootstrap/Button';
+import fetchData from "../../axios";
 
 const customStyles = {
   headRow: {
@@ -28,21 +26,18 @@ const customStyles = {
 };
 
 class CompanyPurBundle extends Component {
-  constructor() {
-    super();
-    this.state = {
-      records: [],
-      filterRecords: [],
-    };
-  }
-  
+  state = {
+    records: [],
+    filterRecords: [],
+  };
+
   handleFilter = (event) => {
     const newData = this.state.filterRecords.filter((row) =>
-    row.name.toLowerCase().includes(event.target.value.toLowerCase())
+      row.name.toLowerCase().includes(event.target.value.toLowerCase())
     );
     this.setState({ records: newData });
   };
-  
+
   componentDidMount() {
     let makeRequest = fetchData();
     makeRequest("GET", "/info/get-purchased-bundles")
@@ -62,7 +57,7 @@ class CompanyPurBundle extends Component {
     const columns = [
       {
         name: "ID",
-        selector: (row,idx) => ++idx,
+        selector: (row, idx) => ++idx,
         sortable: true,
       },
       {
@@ -80,68 +75,52 @@ class CompanyPurBundle extends Component {
       },
       {
         name: "Actions",
-        cell: (id) => <a href="https://test.learnforcare.co.uk/bundle/bundle-all"><Button style={{background:"#212a50", color:'#fff'}} variant="">View</Button></a> ,
+        cell: (id) => <a href="https://test.learnforcare.co.uk/bundle/bundle-all"><Button style={{background:"#212a50", color:'#fff'}} variant="">View</Button></a>,
       },
     ];
 
     return (
       <div className="">
-       
-      <div className="dash-shadow">
-      <div style={{position:'relative'}} className=" row g-3   min-vh-100  d-flex justify-content-center mt-20">
-      <h2
-        style={{  
-          color: "#212450",
-          display: "flex",
-          justifyContent: "center",
-          position:'absolute',
-          fontSize: 38,
-        }}
-      >
-       Purchased Bundle
-      </h2>
-        <div style={{ padding: "", backgroundColor: "" }}>
-          {/* <div
-            className="pb-2 smth"
-            style={{ display: "flex", justifyContent: "left" }}
-          >
-            <input
-              type="text"
-              className=""
-              placeholder="Search course..."
-              onChange={this.handleFilter}
+        <div className="dash-shadow">
+          <div style={{ position: 'relative' }} className=" row g-3   min-vh-100  d-flex justify-content-center mt-20">
+            <h2
               style={{
-                padding: "6px 10px",
-                borderColor: "transparent",
-                overflow: "hidden",
+                color: "#212450",
+                display: "flex",
+                justifyContent: "center",
+                position: 'absolute',
+                fontSize: 38,
               }}
-            />
-          </div> */}
-          <div style={{float:'right',marginBottom:'1.4rem'}} className="p-relative d-inline header__search">
-            <form action="">
-              <input style={{ background:'#edeef3',}}
-                className="d-block mr-10"
-                type="text"
-                placeholder="Search..."
-                // value={searchString}
-                // onChange={handleSearch}
+            >
+              Purchased Bundle
+            </h2>
+            <div style={{ padding: "", backgroundColor: "" }}>
+              <div style={{ float: 'right', marginBottom: '1.4rem' }} className="p-relative d-inline header__search">
+                <form action="">
+                  <input style={{ background: '#edeef3', }}
+                    className="d-block mr-10"
+                    type="text"
+                    placeholder="Search..."
+                    // value={searchString}
+                    // onChange={handleSearch}
+                  />
+                  <button type="submit">
+                    <i className="fas fa-search"></i>
+                  </button>
+                </form>
+              </div>
+              <DataTable
+                noDataComponent={"No records to display"}
+                columns={columns}
+                data={this.state.records}
+                customStyles={customStyles}
+                pagination
+                selectableRows
               />
-              <button type="submit">
-                <i className="fas fa-search"></i>
-              </button>
-            </form>
+            </div>
           </div>
-          <DataTable
-          noDataComponent={"No records to display"}
-            columns={columns}
-            data={this.state.records}
-            customStyles={customStyles}
-            pagination
-            selectableRows
-          />
         </div>
-      </div> </div>
-    </div>
+      </div>
     );
   }
 }
