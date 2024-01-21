6342c8887a9d1c8a409258daed3fdd33653407b3
rahul <rahulor2000@gmail.com>
2024-01-21 23:13:47 +0530

    manager individuals page loding in mobile view  updated

--- src/components/Home/ManageOfIndviduals.js
+++ src/components/Home/ManageOfIndviduals.js
@@ -224,7 +224,7 @@ const ManageIndList = () => {
                 <Spinner animation="border" variant="primary" />
               </div>
             )}
-            {records.length <= 0 && <h4 className="no-record-hidden" style={{textAlign: 'center',marginTop:"5rem",}}>No records to display</h4>}
+            {(records.length <= 0 && !pending) && <h4 className="no-record-hidden" style={{textAlign: 'center',marginTop:"5rem",}}>No records to display</h4>}
             <div>
               {searchString
                 ? records
