906e7eb3887e87f27cff9d95485bb54154f0ee29
rahul <rahulor2000@gmail.com>
2024-03-09 20:51:19 +0530

    view course page updated

--- src/components/CourseDetails/CourseDetailsMain.js
+++ src/components/CourseDetails/CourseDetailsMain.js
@@ -87,7 +87,7 @@ function CourseDetailsMain() {
   const makeRequest = fetchData();
   const [course, setCourse] = useState(() => {
     // log
-    makeRequest("GET", `/course/get-single-course-by-id/${slug}`)
+    makeRequest("GET", `/course/get-single-course-by-id/${slug.replace("_"," ")}`)
       .then((res) => {
         let course = res.data.response[0];
         course.aims = course.aims.filter((item) => item != "");
