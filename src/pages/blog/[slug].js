3aaad56634bf94f64910e4b328d58e8874389377
Kiran Johnson <mekiranjohnson@gmail.com>
2024-03-12 19:44:57 +0530

    blog fn

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -1,7 +1,5 @@
 import React from "react";
-
 import BlogDetailsMain from "../../components/BlogDetails/BlogDetailsMain";
-
 import Footer from "../../components/Layout/Footer/Footer";
 import HeaderOpaque from "../../components/Layout/Header/HeaderOpaque";
 import ShareButton from "react-share/lib/ShareButton";
@@ -9,33 +7,24 @@ import NoSSR from "react-no-ssr";
 import FooterPad from "../../components/Layout/Footer/FooterLess";
 import Cta from "../../components/Home/CtaSection";
 
-class BlogDetailss extends React.Component {
-  static async getInitialProps({ query }) {
-    const { slug } = query;
-    return { slug };
-  }
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <NoSSR>
-          <HeaderOpaque pageTitle="Care course, online care course, Childcare, Mental health care courses" descr="Mental health care courses, Healthcare courses, Care certificate courses, online care course, Dementia care, bundle courses, company purchase, blog on care"/>
-        </NoSSR>
-        <main>
-          {/* Blog Main */}
-          <BlogDetailsMain slug={this.props.slug} />
-          {/* Blog Main End */}
-        </main>
-
-        <Footer />
-      </React.Fragment>
-    );
-  }
-}
+const BlogDetailss = ({ slug }) => {
+  return (
+    <React.Fragment>
+      <NoSSR>
+        <HeaderOpaque
+          pageTitle="Care course, online care course, Childcare, Mental health care courses"
+          descr="Mental health care courses, Healthcare courses, Care certificate courses, online care course, Dementia care, bundle courses, company purchase, blog on care"
+        />
+      </NoSSR>
+      <main>
+        {/* Blog Main */}
+        <BlogDetailsMain slug={slug} />
+        {/* Blog Main End */}
+      </main>
+      <Footer />
+    </React.Fragment>
+  );
+};
 
 export default BlogDetailss;
 
