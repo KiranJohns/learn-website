72e28399c75d33d21ae04fef903e4e3a19f4768c
Kiran Johnson <mekiranjohnson@gmail.com>
2024-03-17 17:15:15 +0530

    robots

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -2,7 +2,7 @@ import React from 'react';
 import ErrorMain from '../components/Error/ErrorMain';
 import Footer from '../components/Layout/Footer/Footer';
 import Header from '../components/Layout/Header/Header';
-
+import Head from "next/head";
 
 class Error extends React.Component {
 
@@ -15,6 +15,9 @@ class Error extends React.Component {
     render() {
         return (
             <React.Fragment>
+                 <Head>
+         <meta name="robots" content="noindex,nofollow" />
+          </Head>
             <Header pageTitle="404 Error: Page Not Found" descr="Oops! The page you were looking for couldn't be found"/>
             <ErrorMain />
             <Footer />
--- src/pages/bundle/[slug].js
+++ src/pages/bundle/[slug].js
@@ -2,13 +2,16 @@ import React from 'react';
 import ErrorMain from '../../components/Error/ErrorMain';
 import Footer from '../../components/Layout/Footer/Footer';
 import Header from '../../components/Layout/Header/Header';
-
+import Head from "next/head";
 
 
 
 const Error = () => {
   return (
     <React.Fragment>
+         <Head>
+    <meta name="robots" content="noindex,nofollow" />
+          </Head>
     <Header pageTitle="404 Error: Page Not Found" descr="Oops! The page you were looking for couldn't be found."/>
     <ErrorMain />
     <Footer />
